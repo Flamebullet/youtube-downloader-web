5a789029baca1c2da76d227cd0305a5f879dcc51
Flame <battle4galaxy@gmail.com>
2025-11-11 21:19:28 +0800

    fix

--- index.js
+++ index.js
@@ -147,11 +147,13 @@ app.get('/search', async (req, res) => {
 					result.client = null;
 					result.thumbnail = result.thumbnails[0].url;
 					result.thumbnails = null;
-					result?.channel?.client = null;
-					result?.channel?.shorts = null;
-					result?.channel?.live = null;
-					result?.channel?.videos = null;
-					result?.channel?.playlists = null;
+					if (result.channel != undefined) {
+						result.channel.client = null;
+						result.channel.shorts = null;
+						result.channel.live = null;
+						result.channel.videos = null;
+						result.channel.playlists = null;
+					}
 					result.url = `https://www.youtube.com/watch?v=${result.id}`;
 					result.timestamp = secToStr(result.duration);
 				}
